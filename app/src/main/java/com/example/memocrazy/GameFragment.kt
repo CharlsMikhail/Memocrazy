b5898da0d289ab3bffb9b78fedc24ff354e9afcd
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-06-20 12:58:02 -0500

    Se hizo el adapter del gridd view, con 6*4, se inserto imaganes referenciales

--- app/src/main/java/com/example/memocrazy/GameFragment.kt
+++ app/src/main/java/com/example/memocrazy/GameFragment.kt
@@ -2,10 +2,15 @@ package com.example.memocrazy
 
 import android.os.Bundle
 import android.view.View
+import android.widget.GridView
 import androidx.fragment.app.Fragment
+import com.example.memocrazy.adapter.GridAdapter
 
 class GameFragment : Fragment(R.layout.fragment_game) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val gridView: GridView = view.findViewById(R.id.grid_game)
+        gridView.adapter = GridAdapter(view.context)
+
     }
 }
---
+++ app/src/main/java/com/example/memocrazy/adapter/GridAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.memocrazy.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+
+class GridAdapter(private val mContext: Context) : BaseAdapter() {
+
+    // Lista de recursos de imagen proporcionados por Android
+    private val mThumbIds = arrayOf(
+        android.R.drawable.ic_menu_camera,
+        android.R.drawable.ic_menu_gallery,
+        android.R.drawable.ic_menu_manage,
+        android.R.drawable.ic_menu_help,
+        android.R.drawable.ic_menu_search,
+        android.R.drawable.ic_menu_send,
+        android.R.drawable.ic_menu_view,
+        android.R.drawable.ic_menu_zoom,
+        android.R.drawable.ic_media_ff,
+        android.R.drawable.ic_media_next,
+        android.R.drawable.ic_media_pause,
+        android.R.drawable.ic_media_play,
+        android.R.drawable.ic_media_previous,
+        android.R.drawable.ic_media_rew,
+        android.R.drawable.ic_media_rew,
+        android.R.drawable.ic_menu_add,
+        android.R.drawable.ic_menu_call,
+        android.R.drawable.ic_menu_crop,
+        android.R.drawable.ic_menu_directions,
+        android.R.drawable.ic_menu_send,
+        android.R.drawable.ic_menu_crop,
+        android.R.drawable.ic_menu_directions,
+        android.R.drawable.ic_menu_send,
+        android.R.drawable.ic_menu_send,
+    )
+
+    override fun getCount(): Int {
+        return mThumbIds.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return mThumbIds[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val imageView: ImageView
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = ImageView(mContext)
+            imageView.layoutParams = ViewGroup.LayoutParams(210, 240)
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setPadding(2, 3, 2, 3)
+        } else {
+            imageView = convertView as ImageView
+        }
+        imageView.setImageResource(mThumbIds[position])
+        return imageView
+    }
+}
