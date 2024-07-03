9b78dcbae2a72e0e15ce33a746b08fd82afc827a
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-07-03 01:52:46 -0500

    Se agrego el item para el recicler view.. ahora si continuamos con la BD

--- app/src/main/java/com/example/memocrazy/adapter/BestScoreViewHolder.kt
+++ app/src/main/java/com/example/memocrazy/adapter/BestScoreViewHolder.kt
@@ -1,23 +1,19 @@
 package com.example.memocrazy.adapter
 
 
-import android.graphics.Color
 import android.view.View
-import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memocrazy.R
 import com.example.memocrazy.entities.BestScore
 
 class BestScoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val viewNombre = itemView.findViewById<TextView>(R.id.txt_name_user)
-    private val viewScore = itemView.findViewById<TextView>(R.id.txt_score)
+    private val viewEmailUser = itemView.findViewById<TextView>(R.id.txt_email_user)
+    private val viewScoreUser = itemView.findViewById<TextView>(R.id.txt_score_user)
     fun render(item: BestScore) {
-        viewNombre.text = item.name
-        viewScore.text = item.score.toString()
+        viewEmailUser.text = item.name
+        viewScoreUser.text = item.score.toString()
     }
 
 }
